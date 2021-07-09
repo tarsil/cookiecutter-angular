76c822225c6b331b9b254fa12b17be0255fcd659
Tiago A. Silva <tiago.arasilva@outlook.com>
2021-07-09 11:54:28 +0200

    Reactive FormsModule added

--- {{ cookiecutter.project_name }}/src/app/app-routing.module.ts
+++ {{ cookiecutter.project_name }}/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 const routes: Routes = [
   { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ReactiveFormsModule, FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
